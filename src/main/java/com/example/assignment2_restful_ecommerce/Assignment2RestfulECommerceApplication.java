1077ab37cfe7341035579824debae582725455a1
Nicholas <berubenic@gmail.com>
2024-10-31 21:58:22 -0400

    init

---
+++ src/main/java/com/example/assignment2_restful_ecommerce/Assignment2RestfulECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment2_restful_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2RestfulECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2RestfulECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment2_restful_ecommerce/Assignment2RestfulECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment2_restful_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2RestfulECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
